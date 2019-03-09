d60c0d507a58a3fe02e59e37b34240766f058f7b
Anthony Intino <anthony.intino@web.com>
2019-03-09 14:23:30 -0500

    Initial commit

---
+++ client/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "../styles.css";
+require("@babel/polyfill");
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  /*
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  async callBackendAPI() {
+    const response = await fetch('/api/contacts');
+    const body = await response.json();
+
+    if(response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  }
+  */
+  
+  render() {
+    let data = this.state.data || 'there is no data';
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        // Render the newly fetched data inside of this.state.data 
+        <p className="App-intro">{data}</p>
+      </div>
+    );
+  }
+}
+
+export default (App);
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
