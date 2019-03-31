8e63f32135850dfe21454996a60d83ba4a5f6eb0
Anthony Intino <anthony.intino@web.com>
2019-03-31 19:29:17 -0400

    get data from endpoint, log contacts. DNR

--- client/components/app.js
+++ client/components/app.js
@@ -4,7 +4,42 @@ import ContactList from './ContactList';
 import AddContactModal from './AddContactModal';
 
 export default class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res }))
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI() {
+    
+    fetch('/api/contacts').then((data) => {
+      console.log(data);
+      return data.json();
+    }).then((body) => {
+      console.log(body);
+      this.setState({data: body})
+    });
+
+    //const body = await response.json();
+
+    // if(response.status !== 200) {
+    //   //test this explicitly, have endpoint return error code
+    //   throw Error(body.error)
+    // }
+    // return body;
+  }
+
   render() {
+    console.log('logging the states');
+    console.log(this.state.data);
     return (
       <div>
         <ContactList />
